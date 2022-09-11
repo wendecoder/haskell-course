83011d32d40bea807db3bf572c3e199e8edd967d
Robertino <48034748+rober-m@users.noreply.github.com>
2022-09-10 23:20:21 -0300

    updated homeworks for lesson 01 and 02

---
+++ Homework/Homework01/Homework01.hs
@@ -0,0 +1,16 @@
+
+-- Question 1
+-- Write a multiline comment below.
+
+-- Question 2
+-- Define a function that takes a value and multiplies it by 3.
+
+-- Question 3
+-- Define a function that calculates the area of a circle.
+
+-- Question 4
+-- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+
+-- Question 5
+-- Define a function that checks if the volume of a cylinder is greater than or equal to 42.
+
---
+++ Homework/Homework02/Homework02.hs
@@ -0,0 +1,37 @@
+
+-- Question 1
+-- Add the type signatures for the functions below and then remove the comments and try to compile.
+
+-- f1 x y z = x ** (y/z)
+
+-- f2 x y z = sqrt (x/y - z)
+
+-- f3 x y z = x:((show y) ++ z)
+
+-- f4 x y z = [x > y] ++ [z]
+
+-- f5 x y z = x == (y ++ z)
+
+-- Question 2
+-- Are really all variables in Haskell immutable? Try googling for the answer.
+
+
+-- Question 3
+-- Why should we define type signatures of functions? How can they help you? How can they help others?
+
+
+-- Question 4
+-- Why should you define type signatures for variables? How can they help you?
+
+
+-- Question 5
+-- Are there any functions in Haskell that let you transform one type to the other? Try googling for the answer.
+
+
+-- Question 6
+-- How would you write the prod function from our lesson so that it works for Int and Double? Does the code compile?
+
+
+-- Question 7
+-- Can you also define in Haskell list of lists? Did we showed any example of that? How would you access the inner
+-- most elements?
