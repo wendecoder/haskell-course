10988f9bd4246da56e9727a5f263455a085521c8
Robertino <48034748+rober-m@users.noreply.github.com>
2022-10-14 15:42:42 +0100

    Added structure for Spanish translation

---
+++ ES-translation/Tarea/Tarea01/Tarea01.hs
@@ -0,0 +1,17 @@
+-- Las respuestas están en la rama: solutions-es
+
+-- Pregunta 1
+-- Escribe un comentario multilinea.
+
+-- Pregunta 2
+-- Define una función que toma un valor y lo mulitplica por 3.
+
+-- Pregunta 3
+-- Define una función que calcule el área de un círculo.
+
+-- Pregunta 4
+-- Define una función que calcule el volúmen de un cilindro usando la función previa.
+
+-- Pregunta 5
+-- Define una función que chequee si el volúmen de un cilindro es mayor o igual a 42.
+
